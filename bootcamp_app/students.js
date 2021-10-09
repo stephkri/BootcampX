91a8d35b702cc239e63134170adcfba49b5eea4d
stephkri <stephane.krims@hotmail.ca>
2021-10-09 01:40:08 +0000

    Sanitize input for teachers.js and students.js

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -8,16 +8,18 @@ const pool = new Pool({
 });
 
 const args = process.argv;
-const month = args[2];
-const limit = args[3];
+const cohortName = args[2];
+const limit = args[3] || 5;
+
+const values = [`%${cohortName}%`, limit];
 
 pool.query(`
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${month}%'
-LIMIT ${limit || 5};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,7 +8,7 @@ const pool = new Pool({
 });
 
 const args = process.argv;
-const cohort = args[2];
+const cohort = args[2] ? `%${args[2]}%` : '%JUN04%';
 
 pool.query(`
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
@@ -16,9 +16,9 @@ FROM assistance_requests
 JOIN teachers ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${cohort || 'JUL02'}'
+WHERE cohorts.name LIKE $1
 ORDER BY teacher;
-`)
+`, [cohort])
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
