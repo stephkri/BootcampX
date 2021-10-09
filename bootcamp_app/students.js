05b6872b99732251d34612a4bfce7e93ab7e1666
stephkri <stephane.krims@hotmail.ca>
2021-10-09 00:30:09 +0000

    Add students.js and teachers.js

---
+++ bootcamp_app/students.js
@@ -0,0 +1,25 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+const args = process.argv;
+const month = args[2];
+const limit = args[3];
+
+pool.query(`
+SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+FROM students
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE '%${month}%'
+LIMIT ${limit || 5};
+`)
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+  })
+}).catch(err => console.error('query error', err.stack));
